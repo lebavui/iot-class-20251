9e92bf6d992abd46fee3bce267948f8a3a82695c
Le Ba Vui <lebavui@gmail.com>
2025-11-15 21:50:03 +0700

    Add new code

---
+++ http_client/http_client.ino
@@ -0,0 +1,80 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "S25";
+const char* password = "123456";
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  // Connect to Wi-Fi
+  Serial.printf("Connecting to Wi-Fi: %s\n", ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected to Wi-Fi!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // === Send GET ===
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+
+    String url = "https://postman-echo.com/get?param1=value1&param2=value2";
+    http.begin(url);
+
+    int httpCode = http.GET();
+
+    if (httpCode > 0) {
+      Serial.printf("Response code: %d\n", httpCode);
+
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println("GET response body:");
+        Serial.println(payload);
+      }
+    } else {
+      Serial.printf("Error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+  }
+
+  delay(2000);
+
+  // === Send POST ===
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+
+    String url = "https://postman-echo.com/post";
+    http.begin(url);
+
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    String body = "param1=value1&param2=value2";
+
+    int httpCode = http.POST(body);
+
+    if (httpCode > 0) {
+      Serial.printf("Response code: %d\n", httpCode);
+
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        Serial.println("POST response body:");
+        Serial.println(payload);
+      }
+    } else {
+      Serial.printf("Error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+  }
+
+}
+
+void loop() {
+}
